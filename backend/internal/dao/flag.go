51806ed83c2539ad6ae39ab8e5718b9d503d643b
Kevin Gavino Sugijanto <florentinus.keving@gmail.com>
2022-08-10 07:59:48 +0700

    Add json parameter to Flag DAO

--- backend/internal/dao/flag.go
+++ backend/internal/dao/flag.go
@@ -3,18 +3,18 @@ package dao
 import "database/sql"
 
 type Flag struct {
-	ID			uint32			`db:"id"`
-	ProjectID	uint32			`db:"project_id"`
-	Name		string			`db:"name"`
-	Active	bool				`db:"active"`
-	UpdateTime	sql.NullTime	`db:"update_time"`
+	ID			uint32			`db:"id" json:"id"`
+	ProjectID	uint32			`db:"project_id" json:"project_id"`
+	Name		string			`db:"name" json:"name"`
+	Active	bool				`db:"active" json:"active"`
+	UpdateTime	sql.NullTime	`db:"update_time" json:"updated_at"`
 }
 
 type FlagContext struct {
-	ID			uint32			`db:"id"`
-	FlagID		uint32  		`db:"flag_id"`
-	Name		string			`db:"name"`
-	Condition	string  		`db:"condition"`
-	Value		string			`db:"value"`
-	UpdateTime	sql.NullTime	`db:"update_time"`
+	ID			uint32			`db:"id" json:"id"`
+	FlagID		uint32  		`db:"flag_id" json:"flag_id"`
+	Name		string			`db:"name" json:"name"`
+	Condition	string  		`db:"condition" json:"condition"`
+	Value		string			`db:"value" json:"value"`
+	UpdateTime	sql.NullTime	`db:"update_time" json:"updated_at"`
 }
